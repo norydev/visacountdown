afcd6a6828cb6308b93979ce96e44b0d8e4ea807
Fabien Loup <info@norydev.com>
2015-09-16 14:52:19 +0300

    skeletton

--- app/models/countdown.rb
+++ app/models/countdown.rb
@@ -4,65 +4,69 @@ class Countdown
     @destination = destination
   end
 
-  def entry_has_happened?(date = Date.current, latest_entry = @destination.user.latest_entry)
-    if latest_entry
-      (date - latest_entry).to_i >= 0
-    else
-      false
-    end
+  def time_spent
+    get_time_spent(Date.current, false, @destination.user.latest_entry)
   end
 
-  def time_spent(date = Date.current, future = false, latest_entry = @destination.user.latest_entry)
-    nb_days = 0
-    oldest_date = date - 179
-    user_periods = @destination.periods
-
-    user_periods = remove_too_old(user_periods, oldest_date)
-    user_periods = remove_future(user_periods, date)
-
-    if latest_entry && !future
-      user_periods = remove_overlaps(user_periods, latest_entry)
-    end
-
-    user_periods = user_periods.map do |period|
-      (period.last_day - period.first_day).to_i + 1
-    end
-    nb_days += user_periods.reduce(:+) if user_periods.present?
-
-    if entry_has_happened?(date, latest_entry) && !future
-      nb_days += (date - latest_entry).to_i + 1
-    end
-
-    nb_days
+  def remaining_time
+    get_remaining_time(Date.current, false, @destination.user.latest_entry)
   end
 
-  def remaining_time(date = Date.current, future = false, latest_entry = @destination.user.latest_entry)
-    rt = 0
-
-    if future
-      # start after entry, who is in the future?
-      (date..(date + 89)).each do |day|
-        rt += 1 if time_spent(day + 1, future, latest_entry) + rt <= 90
-      end
-      rt -= 1
-    else # elsif self.entry_has_happened?(date, latest)
-      # start after entry, who is in the past
-      (date..(latest_entry + 89)).each do |day|
-        rt += 1 if time_spent(day + 1, future, latest_entry) <= 90
-      end
-    end
-    rt
-  end
-
-  def next_entry(date = Date.current)
-    wt = 0
-    (date..(date + 90)).each do |day|
-      wt += 1 if time_spent(day, true) >= 90
-    end
-    date + wt
-  end
+  # def next_entry(date = Date.current)
+  #   wt = 0
+  #   (date..(date + 90)).each do |day|
+  #     wt += 1 if get_time_spent(day, true) >= 90
+  #   end
+  #   date + wt
+  # end
 
   private
+
+    def entry_has_happened?
+      @destination.user.latest_entry.try(:past?)
+    end
+
+    def get_time_spent(date = Date.current, future = false, latest_entry = @destination.user.latest_entry)
+      nb_days = 0
+      oldest_date = date - 179
+      user_periods = @destination.periods
+
+      user_periods = remove_too_old(user_periods, oldest_date)
+      user_periods = remove_future(user_periods, date)
+
+      if latest_entry && !future
+        user_periods = remove_overlaps(user_periods, latest_entry)
+      end
+
+      user_periods = user_periods.map do |period|
+        (period.last_day - period.first_day).to_i + 1
+      end
+      nb_days += user_periods.reduce(:+) if user_periods.present?
+
+      if entry_has_happened?(date, latest_entry) && !future
+        nb_days += (date - latest_entry).to_i + 1
+      end
+
+      nb_days
+    end
+
+    def get_remaining_time(date = Date.current, future = false, latest_entry = @destination.user.latest_entry)
+      rt = 0
+
+      if future
+        # start after entry, who is in the future?
+        (date..(date + 89)).each do |day|
+          rt += 1 if get_time_spent(day + 1, future, latest_entry) + rt <= 90
+        end
+        rt -= 1
+      else # elsif self.entry_has_happened?(date, latest)
+        # start after entry, who is in the past
+        (date..(latest_entry + 89)).each do |day|
+          rt += 1 if get_time_spent(day + 1, future, latest_entry) <= 90
+        end
+      end
+      rt
+    end
 
     def remove_too_old(periods, oldest_date)
       # remove if period is entirely before oldest date
