01f1d3bda22998cecc6ec8219f4df35d833dad29
Fabien Loup <info@norydev.com>
2015-09-16 15:55:58 +0300

    correct country

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ Destination.all.each do |d|
     start = Faker::Date.backward(220)
     p.first_day = start
     p.last_day = start + 20
-    p.country = ["Turkey", "Switzerland", "Belgium"].sample
+    p.country = d.zone == "Turkey" ? "Turkey" : SCHENGEN.sample
     p.destination = d
     p.save
   end
