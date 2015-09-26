5fcc3c9bd50790290dcfac75522ecd51bb147656
Fabien Loup <info@norydev.com>
2015-09-27 00:03:17 +0300

    dashboard

--- app/helpers/destinations_helper.rb
+++ app/helpers/destinations_helper.rb
@@ -1,18 +1,39 @@
 module DestinationsHelper
-  def display_countdown(countdown)
+  def display_countdown_for(destination)
+    countdown = destination.countdown
     case countdown.situation
     when "inside_ok"
-      "<ul class=list-unstyled>
-        <li>Time spent in the last 180 days: <strong>#{countdown.time_spent}</strong> days</li>
-        <li>Remaining time: <strong>#{countdown.remaining_time}</strong> days</li>
-        <li>Latest exit date: <strong>#{date_format(countdown.exit_day)}</strong></li>
-      </ul>".html_safe
+      "<div class='panel panel-success'>
+        <div class='panel-heading'>You are in #{destination}</div>
+        <div class='panel-body'>
+          <ul class=list-unstyled>
+            <li>Time spent in the last 180 days: <strong>#{countdown.time_spent}</strong> days</li>
+            #{
+              if destination.latest_entry && destination.latest_entry <= Date.current
+                'You entered on ' + date_format(destination.latest_entry)
+              elsif destination.latest_entry && destination.latest_entry > Date.current
+                'You plan to enter on ' + date_format(destination.latest_entry)
+              else
+                '</ul><ul class=list-unstyled><li>If you enter tomorrow:</li>'.html_safe
+              end
+            }
+            <li>Remaining time: <strong>#{countdown.remaining_time}</strong> days</li>
+            <li>Latest exit date: <strong>#{date_format(countdown.exit_day)}</strong></li>
+          </ul>
+        </div>
+      </div>".html_safe
     when "outside_ok"
-      "<ul class=list-unstyled>
-        <li>Time spent in the last 180 days: <strong>#{countdown.time_spent}</strong> days</li>
-        <li>Remaining time: <strong>#{countdown.remaining_time}</strong> days</li>
-        <li>Latest exit date: <strong>#{date_format(countdown.exit_day)}</strong></li>
-      </ul>".html_safe
+      "<div class='panel panel-success'>
+        <div class='panel-heading'>You are outside of #{destination}</div>
+        <div class='panel-body'>
+          <ul class=list-unstyled>
+            <li>Time spent in the last 180 days: <strong>#{countdown.time_spent}</strong> days</li>
+            #{'</ul><ul class=list-unstyled><li>If you enter tomorrow:</li>'.html_safe unless destination.latest_entry}
+            <li>Remaining time: <strong>#{countdown.remaining_time}</strong> days</li>
+            <li>Latest exit date: <strong>#{date_format(countdown.exit_day)}</strong></li>
+          </ul>
+        </div>
+      </div>".html_safe
     when "overstay"
     when "current_too_long"
     when "one_next_too_long"
