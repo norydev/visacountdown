2f247a01fcee7fdb5df236dcdda73ac38edd5de7
Fabien Loup <info@norydev.com>
2015-10-12 19:27:46 +0300

    added authorizations, test pass

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,21 +3,25 @@ class DestinationsController < ApplicationController
   before_action :set_destination
 
   def edit
+    user_not_authorized unless current_or_guest_user.destinations.include?(@destination)
   end
 
   # PATCH
   def set_latest_entry
-    # update for FE-FW
-    if @destination.update(latest_entry_params)
-      respond_to do |format|
-        format.html { redirect_to root_path, notice: 'Your latest location has been updated.' }
-        format.js
+    if current_or_guest_user.destinations.include?(@destination)
+      if @destination.update(latest_entry_params)
+        respond_to do |format|
+          format.html { redirect_to root_path, notice: 'Your latest location has been updated.' }
+          format.js
+        end
+      else
+        respond_to do |format|
+          format.html { render :edit }
+          format.js
+        end
       end
     else
-      respond_to do |format|
-        format.html { render :edit }
-        format.js
-      end
+      user_not_authorized
     end
   end
 
